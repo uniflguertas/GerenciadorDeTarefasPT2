df1862b6d7cd52de30791dfe5814c84f38bc8ddc
uniflguertas <149299894+uniflguertas@users.noreply.github.com>
2023-11-12 22:00:33 -0300

    Update proj.h

--- proj.h
+++ proj.h
@@ -3,7 +3,7 @@ typedef struct {
     int prioridade;
     char categoria[100];
     char descricao[300];
-    int estado;
+        int estado;
 } Tarefa;
 
 // Declaração das funções do "proj.c":
@@ -11,3 +11,5 @@ void cadastrarTarefa(Tarefa tarefas[], int *numTarefas);
 void deletarTarefa(Tarefa tarefas[], int *numTarefas);
 void listarTarefas(Tarefa tarefas[], int numTarefas);
 void alterarTarefa(Tarefa tarefas[], int numTarefas);
+int exportarTarefas(Tarefa tarefas[], int numTarefas);
+int compararPrioridade(const void *a, const void *b);
