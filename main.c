6cb69770f3ce26436b730d0c594aaf664c0108ad
uniflguertas <149299894+uniflguertas@users.noreply.github.com>
2023-11-12 23:03:54 -0300

    ajustes mínimos em main.c

--- main.c
+++ main.c
@@ -6,18 +6,15 @@ int main(){
 
 //  Limite do número de tarefas cadastráveis:
     Tarefa tarefas[100];
-
 //  Variável que determina o número de tarefas cadastradas:
     int numTarefas = 0;
-
-
 //  Variável usada para detectar qual opção será utilizada:
     int opcao;
 
     // Algoritmo de escolha de opções:
-    //   Neste trecho está armazenado o algoritmo onde o computador irá executar suas funcionalidades de acordo com a atividade do usuário.
+    // Neste trecho está armazenado o algoritmo onde o computador irá executar suas funcionalidades de acordo com a atividade do usuário.
     do {
-        // Menu Principal:
+     // Menu Principal:
         printf("\nBem-vindo ao Gerenciador de Tarefas!\n");
         printf("\n");
         printf("Qual funcionalidade gostaria de performar hoje?\n");
@@ -30,36 +27,36 @@ int main(){
         printf("\nDigite aqui o algarismo da funcionalidade desejada: ");
         scanf("%d", &opcao);
 
-        // Se o input = 0, encerra o programa.
+     // Se o input = 0, encerra o programa.
         if(opcao == 0){
             printf("\n");
             printf("Encerrando Gerenciador...\n");
         }
 
-            // Se o input do usuário = 1, cadastra uma nova tarefa.
+     // Se o input do usuário = 1, cadastra uma nova tarefa.
         else if(opcao == 1){
             cadastrarTarefa(tarefas, &numTarefas);
         }
 
-            // Se o input do usuário = 2, deleta uma tarefa de acordo com seu respectivo número.
+     // Se o input do usuário = 2, deleta uma tarefa de acordo com seu respectivo número.
         else if(opcao == 2){
             deletarTarefa(tarefas, &numTarefas);
         }
 
-            //se o input do usuário = 3, listar as tarefas cadastradas.
+     // Se o input do usuário = 3, listar as tarefas cadastradas.
         else if(opcao == 3){
             listarTarefas(tarefas, numTarefas);
         }
-
+     // Se o input do usuário = 4, alterar uma tarefa existente.
         else if(opcao == 4){
             alterarTarefa(tarefas, numTarefas);
         }
-
+     // Se o input do usuário = 5, exportar tarefas.
         else if(opcao == 5){
             exportarTarefas(tarefas, numTarefas);
         }
 
-            //se o input do usuário não for nenhuma das opções acima, retorna erro.
+     // Se o input do usuário não for nenhuma das opções acima, retorna erro.
         else{
             printf("\n");
             printf("ERRO: Funcionalidade inexistente.\n");
@@ -69,7 +66,7 @@ int main(){
     } while (opcao != 0);
 //     ^ manter o loop enquanto opção != 0.
 
-// Comando de acréscimo/deleção e listagem dentro do arquivo binário:
+ // Comando de acréscimo/deleção e listagem dentro do arquivo binário:
     FILE *file = fopen("tarefas.bin", "wb");
     if (file) {
         fwrite(tarefas, sizeof(Tarefa), numTarefas, file);
